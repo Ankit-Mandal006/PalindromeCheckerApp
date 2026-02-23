156b108c50ec810683b8d1395939c0289ddef299
Ankit-Mandal006 <mandal.ankit190506@gmail.com>
2026-02-23 09:23:54 +0530

    Update

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -11,8 +11,8 @@ public class PalindromeCheckerApp {
        for(int i=0;i<wrd.length();i++)
             temp=wrd.charAt(i)+temp;
        if(temp.equals(wrd))
-           System.out.println("Palindrome");
+           System.out.println(wrd +" is Palindrome");
        else
-           System.out.println("Not Palindrome");
+           System.out.println(wrd +" is Not Palindrome");
     }
 }
